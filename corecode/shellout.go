4c62e578a764457bfffdc76c8d2d9bd30775e331
AbhishekKr <abhikumar163@gmail.com>
2017-01-30 00:11:22 +0530

    [corecode] shellout got custom error for empty command
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- corecode/shellout.go
+++ corecode/shellout.go
@@ -2,6 +2,7 @@ package dolores_corecode
 
 import (
 	"bytes"
+	"errors"
 	"log"
 	"os/exec"
 )
@@ -10,12 +11,14 @@ func Exec(commandTokens []string) (err error) {
 	token_count := len(commandTokens)
 	var cmd *exec.Cmd
 	if token_count == 0 {
-		log.Fatalln("[ERROR] No command to run")
+		log.Println("[ERROR] Exec got no command to run")
+		err = errors.New("Exec got no command to run")
 	} else if token_count == 1 {
 		cmd = exec.Command(commandTokens[0])
 	} else {
 		cmd = exec.Command(commandTokens[0], commandTokens[1:]...)
 	}
+
 	var out bytes.Buffer
 	cmd.Stdout = &out
 	err = cmd.Run()
