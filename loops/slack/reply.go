b11cb9a542a5a076089a4fdd7481c09060c0454c
AbhishekKr <abhikumar163@gmail.com>
2017-01-31 22:28:06 +0530

    [loop] handle error case for getUserInfo while replying
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

--- loops/slack/reply.go
+++ loops/slack/reply.go
@@ -7,12 +7,16 @@ import (
 )
 
 func Reply(event *slack.MessageEvent, message string) (err error) {
+	userName := event.Msg.User
 	user, err := API.GetUserInfo(event.Msg.User)
+	if err == nil {
+		userName = user.Name
+	}
 	params := slack.PostMessageParameters{}
 	params.Username = BotID
 	params.AsUser = true
 	params.LinkNames = 1 // so slack linkify channel names and usernames https://api.slack.com/docs/message-formatting
-	replyMessage := fmt.Sprintf("@%s: %s", user.Name, message)
+	replyMessage := fmt.Sprintf("@%s: %s", userName, message)
 	API.PostMessage(event.Msg.Channel, replyMessage, params)
 	return
 }
