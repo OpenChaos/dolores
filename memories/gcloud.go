93e9499a6e4c61ac569f2a2ffcfb25511e75abe6
AbhishekKr <abhikumar163@gmail.com>
2017-02-14 02:58:25 +0530

    [memories] shell-exec for GcloudComputeInstances with a parent-dir to store results
    Signed-off-by: AbhishekKr <abhikumar163@gmail.com>

---
+++ memories/gcloud.go
@@ -0,0 +1,20 @@
+package dolores_memories
+
+import (
+	dolores_corecode "dolores/corecode"
+	"log"
+	"os"
+)
+
+func GcloudComputeInstances() {
+	log.Println("[memory] remember GCloud instances")
+	working_dir, _ := os.Getwd()
+	parameters := []string{
+		dolores_corecode.OverrideFromEnvVar("DOLORES_SHELL_OUTPUT_DIR", working_dir),
+	}
+	output, err := dolores_corecode.Exec("gcloud-compute-instances", parameters[0:]...)
+	log.Println(output)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
